6675bb9fabe70bb4abedf3b8b1cbf32ac96efe28
Okta P <57696739+oktaaaa@users.noreply.github.com>
2023-12-02 14:01:58 +0700

    .

--- src/Fakultas/List.js
+++ src/Fakultas/List.js
@@ -1,5 +1,18 @@
-import React from "react";
+import { Axios } from "axios";
+import { error } from "console";
+import React, { useEffect } from "react";
 const List = () =>{
+    const [fakultas, setFakultas] = useState([])
+
+    useEffect(() => {
+        Axios.get("https://apimi5a.vercel.app/fakultas")
+        .then((res) => {
+            setFakultas(res)
+        }).catch((error) => {
+            alert(error)
+        })
+    }, [])
+
     return(
         <>
             <h2>List</h2>

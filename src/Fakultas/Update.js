f10e6208f216527b112fc2f49b56c4a70da2281a
ribka123 <ribkalauren@mhs.mdp.ac.id>
2023-12-09 13:58:47 +0700

    list dan update

--- src/Fakultas/Update.js
+++ src/Fakultas/Update.js
@@ -1,8 +1,57 @@
-import React from "react";
+import Axios  from "axios";
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+
 const Update = () =>{
+    const navigate = useNavigate()
+    const {fakultasId} = useParams()
+    const [fakultas, setFakultas] = useState({nama: ""})
+
+    const handleChange = (e, name) => {
+        const value = e.target.value
+        setFakultas({...fakultas, [name]:value})
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        try {
+            await Axios.put(`https://apimi5a.vercel.app/fakultas/${fakultasId}`)
+                .then((res) => {
+                    navigate("/fakultas")
+                })
+        } catch (error) {
+            alert(error)
+        }
+    }
+    useEffect(() => {
+        const getFakultas = async () =>{
+            try {
+                await Axios.get(`https://apimi5a.vercel.app/fakultas/${fakultasId}`)
+                .then((res) => {
+                    const {data} = res
+                    console.log(data);
+                    setFakultas ({
+                        nama: data?.nama
+                    })
+                })
+            } catch (error) {
+                alert(error)
+            }
+           
+        }
+        getFakultas()
+    }, [fakultasId])
+
+    
     return(
         <>
-            <h2>Home</h2>
+            <h2>Halaman Update Fakultas</h2>
+            <form>
+                <input type="text" value={fakultas.nama} onChange={(e) => handleChange(e, "nama")}
+                className="form-control" placeholder="Input Nama Fakultas"/>
+                <button className="btn btn-primary">Simpan</button>
+                <button className="btn btn-light" onClick={() => navigate("/fakultas")}>Kembali</button>
+            </form>
         </>
     )
 }
